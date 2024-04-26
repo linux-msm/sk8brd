091d4a77b73d19c57d5a0dfe0e94db06554b20f2
Konrad Dybcio <konradybcio@kernel.org>
2024-04-26 03:26:22 +0200

    initial commit

---
+++ src/lib.rs
@@ -0,0 +1,140 @@
+use serde::{Deserialize, Serialize};
+use std::cmp::min;
+use std::io::{stdout, Write};
+use std::mem::size_of;
+
+#[repr(u8)]
+pub enum Sk8brdMsgs {
+    MsgSelectBoard = 1,
+    MsgConsole,
+    MsgHardReset,
+    MsgPowerOn,
+    MsgPowerOff,
+    MsgFastbootPresent,
+    MsgFastbootDownload,
+    MsgFastbootBoot,
+    MsgStatusUpdate,
+    MsgVbusOn,
+    MsgVbusOff,
+    MsgFastbootReboot,
+    MsgSendBreak,
+    MsgListDevices,
+    MsgBoardInfo,
+    MsgFastbootContinue,
+}
+
+pub fn u8_to_msg(n: u8) -> Sk8brdMsgs {
+    match n {
+        1 => Sk8brdMsgs::MsgSelectBoard,
+        2 => Sk8brdMsgs::MsgConsole,
+        3 => Sk8brdMsgs::MsgHardReset,
+        4 => Sk8brdMsgs::MsgPowerOn,
+        5 => Sk8brdMsgs::MsgPowerOff,
+        6 => Sk8brdMsgs::MsgFastbootPresent,
+        7 => Sk8brdMsgs::MsgFastbootDownload,
+        8 => Sk8brdMsgs::MsgFastbootBoot,
+        9 => Sk8brdMsgs::MsgStatusUpdate,
+        10 => Sk8brdMsgs::MsgVbusOn,
+        11 => Sk8brdMsgs::MsgVbusOff,
+        12 => Sk8brdMsgs::MsgFastbootReboot,
+        13 => Sk8brdMsgs::MsgSendBreak,
+        14 => Sk8brdMsgs::MsgListDevices,
+        15 => Sk8brdMsgs::MsgBoardInfo,
+        16 => Sk8brdMsgs::MsgFastbootContinue,
+        _ => panic!("Illegal u8 -> Sk8brdMsg conversion"),
+    }
+}
+
+#[derive(Copy, Clone, Debug, Deserialize, Serialize)]
+#[repr(C)]
+#[repr(packed(1))]
+pub struct Sk8brdMsg {
+    pub r#type: u8,
+    pub len: u16,
+}
+pub const MSG_HDR_SIZE: usize = size_of::<Sk8brdMsg>();
+
+pub fn send_msg(write_sink: &mut impl Write, r#type: Sk8brdMsgs, size: usize, buf: &[u8]) {
+    let msg: Sk8brdMsg = Sk8brdMsg {
+        r#type: r#type as u8,
+        len: size as u16,
+    };
+
+    write_sink
+        .write_all(&bincode::serialize(&msg).unwrap())
+        .unwrap();
+    let _ = write_sink.write(&buf[0..size]).unwrap();
+}
+
+pub fn send_ack(write_sink: &mut impl Write, r#type: Sk8brdMsgs) {
+    send_msg(write_sink, r#type, 0, &[0])
+}
+
+pub fn parse_recv_msg(buf: &[u8]) -> Sk8brdMsg {
+    let msg: Sk8brdMsg = Sk8brdMsg {
+        r#type: buf[0],
+        len: (buf[2] as u16) << 8 | buf[1] as u16,
+    };
+
+    // println!("{:?}", msg);
+
+    msg
+}
+
+pub fn console_print(buf: &[u8], len: u16) {
+    print!("{}", String::from_utf8_lossy(&buf[..len as usize]));
+    stdout().flush().unwrap();
+}
+
+#[allow(clippy::explicit_write)]
+pub fn send_image(write_sink: &mut impl Write, buf: &Vec<u8>) {
+    let mut last_percent_done: usize = 0;
+    let mut bytes_sent = 0;
+
+    while bytes_sent < buf.len() - 1 {
+        let bytes_left = min(2048, buf.len() - bytes_sent);
+        let percent_done = 100 * bytes_sent / buf.len();
+
+        if percent_done != last_percent_done {
+            writeln!(stdout(), " Sending image: {}%\r", percent_done).unwrap();
+        }
+
+        send_msg(
+            write_sink,
+            Sk8brdMsgs::MsgFastbootDownload,
+            bytes_left,
+            &buf[bytes_sent..],
+        );
+
+        bytes_sent += bytes_left - 1;
+        last_percent_done = percent_done;
+    }
+
+    send_ack(write_sink, Sk8brdMsgs::MsgFastbootDownload)
+}
+
+pub fn select_brd(write_sink: &mut impl Write, name: &str) {
+    send_msg(
+        write_sink,
+        Sk8brdMsgs::MsgSelectBoard,
+        name.len(),
+        name.as_bytes(),
+    )
+}
+
+#[allow(clippy::explicit_write)]
+pub fn list_device(buf: &[u8], len: u16) {
+    if len == 0 {
+        return;
+    }
+
+    writeln!(
+        stdout(),
+        "{}\r",
+        String::from_utf8_lossy(&buf[..len as usize])
+    )
+    .unwrap();
+    stdout().flush().unwrap();
+}
+
+pub fn list_boards() {}
---
+++ src/main.rs
@@ -0,0 +1,145 @@
+use clap::Parser;
+use colored::Colorize;
+use sk8brd::{
+    console_print, list_device, parse_recv_msg, select_brd, send_ack, send_image, send_msg,
+    u8_to_msg, Sk8brdMsgs, MSG_HDR_SIZE,
+};
+use ssh2::Session;
+use std::fs;
+use std::io::{stdout, Read, Write};
+use std::net::TcpStream;
+use std::sync::Arc;
+use tokio::sync::Mutex;
+
+const SSH_BUFFER_SIZE: usize = 2048;
+const CDBA_SERVER_BIN_NAME: &str = "cdba-server";
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[arg(short)]
+    farm: String,
+
+    #[arg(short, default_value_t = String::from("22"))]
+    port: String,
+
+    #[arg(short)]
+    board: String,
+
+    #[arg(short)]
+    image_path: String,
+
+    #[arg(short, default_value_t = String::from("cdba"))]
+    user: String,
+}
+
+async fn handle_keypress(c: char, quit: &mut Arc<Mutex<bool>>) {
+    if true {
+        match c {
+            'q' => *quit.lock().await = true,
+            'Q' => *quit.lock().await = true,
+            _ => (),
+        }
+    }
+}
+
+// For raw mode TTY
+#[allow(clippy::explicit_write)]
+#[tokio::main]
+async fn main() {
+    let mut hdr_buf = [0u8; MSG_HDR_SIZE];
+    let mut buf = [0u8; SSH_BUFFER_SIZE];
+    let mut key_buf = [0u8; 1];
+    let quit = Arc::new(Mutex::new(false));
+    let args = Args::parse();
+
+    let fastboot_image = fs::read(args.image_path).expect("boot image not found");
+
+    // Connect to the local SSH server
+    let tcp = match TcpStream::connect(format!("{}:{}", args.farm, args.port)) {
+        Ok(t) => t,
+        Err(e) => panic!("Server connection failed: {e}"),
+    };
+    let mut sess = Session::new().unwrap();
+    sess.set_tcp_stream(tcp);
+    match sess.handshake() {
+        Ok(s) => s,
+        Err(e) => panic!("SSH handshake failed: {e}"),
+    };
+
+    // Try to authenticate with the first identity in the agent.
+    match sess.userauth_agent("cdba") {
+        Ok(s) => s,
+        Err(e) => panic!("SSH agent authentication failed: {e}"),
+    };
+
+    let mut chan = sess.channel_session().unwrap();
+    chan.exec(CDBA_SERVER_BIN_NAME).unwrap();
+
+    sess.set_blocking(false);
+
+    send_msg(&mut chan, Sk8brdMsgs::MsgListDevices, 0, &[0]);
+    select_brd(&mut chan, &args.board);
+
+    crossterm::terminal::enable_raw_mode().unwrap();
+
+    let mut quit2 = Arc::clone(&quit);
+    let stdin_handler = tokio::spawn(async move {
+        let mut stdin = os_pipe::dup_stdin().expect("Couldn't dup stdin");
+
+        loop {
+            if stdin.read(&mut key_buf).unwrap() > 0 {
+                handle_keypress(key_buf[0] as char, &mut quit2).await;
+            };
+        }
+    });
+
+    while !*quit.lock().await {
+        // Stream of "blue text" - status updates from the server
+        let bytes_read = chan.stderr().read(&mut buf).unwrap_or(0);
+        if bytes_read > 0 {
+            let s = String::from_utf8_lossy(&buf[..bytes_read]);
+            writeln!(stdout(), "{}\r", s.blue()).unwrap();
+            stdout().flush().unwrap();
+        }
+
+        // Msg handler
+        // Read the message header first
+        if chan.read_exact(&mut hdr_buf).is_ok() {
+            sess.set_blocking(true);
+            let msg = parse_recv_msg(&hdr_buf);
+
+            // Now read the actual data
+            chan.read_exact(&mut buf[..msg.len as usize]).unwrap();
+
+            // And process it
+            match u8_to_msg(msg.r#type) {
+                Sk8brdMsgs::MsgSelectBoard => send_msg(&mut chan, Sk8brdMsgs::MsgPowerOn, 0, &[0]),
+                Sk8brdMsgs::MsgConsole => console_print(&buf, msg.len),
+                Sk8brdMsgs::MsgPowerOn => (),
+                Sk8brdMsgs::MsgFastbootPresent => send_image(&mut chan, &fastboot_image),
+                Sk8brdMsgs::MsgListDevices => list_device(&buf, msg.len),
+                _ => println!("unknown msg: {:?}", msg),
+            };
+            sess.set_blocking(false);
+        }
+    }
+
+    // No more keypresses will be useful
+    stdin_handler.abort();
+
+    // Pick up the trash
+    crossterm::terminal::disable_raw_mode().unwrap();
+
+    // Power off the board on goodbye
+    send_ack(&mut chan, Sk8brdMsgs::MsgPowerOff);
+
+    sess.disconnect(
+        Option::Some(ssh2::DisconnectCode::ConnectionLost),
+        "bye",
+        Option::Some("C"),
+    )
+    .unwrap();
+
+    println!("Goodbye");
+}
