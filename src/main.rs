143159b868e5275c240d3f3b70a7e6964fbc4a8e
Konrad Dybcio <konradybcio@kernel.org>
2024-05-07 01:44:32 +0200

    main+lib: Locky lock, implement console writing
    Signed-off-by: Konrad Dybcio <konradybcio@kernel.org>

--- src/main.rs
+++ src/main.rs
@@ -38,12 +38,26 @@ struct Args {
     power_cycle: bool,
 }
 
-async fn handle_keypress(c: char, quit: &mut Arc<Mutex<bool>>) {
-    if true {
+async fn handle_keypress(
+    c: char,
+    quit: &mut Arc<Mutex<bool>>,
+    special: &mut bool,
+    message_sink: &mut Arc<Mutex<impl Write>>,
+) {
+    if *special {
+        *special = false;
         match c {
             'q' => *quit.lock().await = true,
             'Q' => *quit.lock().await = true,
             _ => (),
+        }
+    } else {
+        match c.try_into() {
+            Ok(1u8) => *special = true, // CTRL-A, TODO: configurable?
+            Ok(_) => send_msg(message_sink, Sk8brdMsgs::MsgConsole, &[c as u8])
+                .await
+                .unwrap(),
+            Err(_) => (),
         }
     }
 }
@@ -52,6 +66,12 @@ macro_rules! todo {
     ($s: expr) => {{
         let val = format!($s);
         writeln!(stdout(), "{val}\r").unwrap();
+    }};
+}
+
+macro_rules! get_arc {
+    ($a: expr) => {{
+        $a.lock().await
     }};
 }
 
@@ -80,29 +100,32 @@ async fn main() -> anyhow::Result<()> {
     sess.userauth_agent(USERNAME)
         .with_context(|| format!("Couldn't authenticate as {USERNAME}"))?;
 
-    let mut chan = sess.channel_session()?;
-    chan.exec(CDBA_SERVER_BIN_NAME)
+    let mut chan = Arc::new(Mutex::new(sess.channel_session()?));
+    (*get_arc!(chan))
+        .exec(CDBA_SERVER_BIN_NAME)
         .with_context(|| format!("Couldn't execute {CDBA_SERVER_BIN_NAME} on remote host"))?;
 
     sess.set_blocking(false);
 
-    send_ack(&mut chan, Sk8brdMsgs::MsgListDevices)?;
-    select_brd(&mut chan, &args.board)?;
+    send_ack(&mut chan, Sk8brdMsgs::MsgListDevices).await?;
+    select_brd(&mut chan, &args.board).await?;
     if args.power_cycle {
         println!("Powering off the board first");
-        send_ack(&mut chan, Sk8brdMsgs::MsgPowerOff)?;
+        send_ack(&mut chan, Sk8brdMsgs::MsgPowerOff).await?;
     }
 
     crossterm::terminal::enable_raw_mode()?;
 
     let mut quit2 = Arc::clone(&quit);
+    let mut chan2 = Arc::clone(&chan);
     let stdin_handler = tokio::spawn(async move {
         let mut stdin = os_pipe::dup_stdin().expect("Couldn't dup stdin");
+        let mut ctrl_a_pressed = false;
 
         while !*quit2.lock().await {
             if let Ok(len) = stdin.read(&mut key_buf) {
                 for c in key_buf[0..len].iter() {
-                    handle_keypress(*c as char, &mut quit2).await;
+                    handle_keypress(*c as char, &mut quit2, &mut ctrl_a_pressed, &mut chan2).await;
                 }
             };
         }
@@ -110,7 +133,7 @@ async fn main() -> anyhow::Result<()> {
 
     while !*quit.lock().await {
         // Stream of "blue text" - status updates from the server
-        if let Ok(bytes_read) = chan.stderr().read(&mut buf) {
+        if let Ok(bytes_read) = (*get_arc!(chan)).stderr().read(&mut buf) {
             let s = String::from_utf8_lossy(&buf[..bytes_read]);
             writeln!(stdout(), "{}\r", s.blue())?;
             stdout().flush()?;
@@ -118,24 +141,26 @@ async fn main() -> anyhow::Result<()> {
 
         // Msg handler
         // Read the message header first
-        if chan.read_exact(&mut hdr_buf).is_ok() {
+        if (*get_arc!(chan)).read_exact(&mut hdr_buf).is_ok() {
             sess.set_blocking(true);
             let msg = parse_recv_msg(&hdr_buf);
             let mut msgbuf = vec![0u8; msg.len as usize];
 
             // Now read the actual data...
-            chan.read_exact(&mut msgbuf)?;
+            (*get_arc!(chan)).read_exact(&mut msgbuf)?;
 
             // ..and process it
             match msg.r#type.try_into() {
-                Ok(Sk8brdMsgs::MsgSelectBoard) => send_msg(&mut chan, Sk8brdMsgs::MsgPowerOn, &[])?,
-                Ok(Sk8brdMsgs::MsgConsole) => console_print(&msgbuf),
+                Ok(Sk8brdMsgs::MsgSelectBoard) => {
+                    send_msg(&mut chan, Sk8brdMsgs::MsgPowerOn, &[]).await?
+                }
+                Ok(Sk8brdMsgs::MsgConsole) => console_print(&msgbuf).await,
                 Ok(Sk8brdMsgs::MsgHardReset) => todo!("MsgHardReset is unused"),
                 Ok(Sk8brdMsgs::MsgPowerOn) => (),
                 Ok(Sk8brdMsgs::MsgPowerOff) => (),
                 Ok(Sk8brdMsgs::MsgFastbootPresent) => {
                     if !msgbuf.is_empty() && msgbuf[0] != 0 {
-                        send_image(&mut chan, &fastboot_image)?
+                        send_image(&mut chan, &fastboot_image).await?
                     }
                 }
                 Ok(Sk8brdMsgs::MsgFastbootDownload) => (),
@@ -163,7 +188,7 @@ async fn main() -> anyhow::Result<()> {
     crossterm::terminal::disable_raw_mode()?;
 
     // Power off the board on goodbye
-    send_ack(&mut chan, Sk8brdMsgs::MsgPowerOff)?;
+    send_ack(&mut chan, Sk8brdMsgs::MsgPowerOff).await?;
 
     sess.disconnect(
         Option::Some(ssh2::DisconnectCode::ConnectionLost),
